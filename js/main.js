c7877b14076d03176d055d5d084fae20a33a3173
Marat Daudov <sharedhare@gmail.com>
2019-01-30 15:39:44 +0600

    some fixes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
-// $(window).scroll(function () {
-//     var height = $(window).scrollTop();
-
-//     /*Если сделали скролл на 100px задаём новый класс для  header-mb*/
-//     if (height > 100) {
-//         $('.header .g-wrapper .g-content').addClass('header-fixed');
-//         $('.header').addClass('header-mb');
-//     } else {
-//         /*Если меньше 100px удаляем класс для header-mb*/
-//         $('.header .g-wrapper .g-content').removeClass('header-fixed');
-//         $('.header').removeClass('header-mb');
-//     }
-// });
 var range_2_val = 500000, range_3_val = 36
 
 $(document).ready(function () {
-    $(".requirements_btn--link_1").click(function () { // элемент клика
+	$(".requirements_btn--link_1").click(function () { // элемент клика
 		$(".requirements_btn--link_1").toggleClass('requirements_btn--active');
 		$(".requirements_btn--link_2").removeClass('requirements_btn--active');
 		$(".requirements_list.link_1--mob").toggleClass('requirements_list--active');
@@ -67,28 +54,28 @@ $(document).ready(function () {
 		}
 	});
 
-    $("#range-02-text").on('change',function() {
-        $("#range-02").data("ionRangeSlider").update({
-            from: $("#range-02-text").val().replace(/[\D\s\._\-]+/g, "")
-        });
-    });
+	$("#range-02-text").on('change', function () {
+		$("#range-02").data("ionRangeSlider").update({
+			from: $("#range-02-text").val().replace(/[\D\s\._\-]+/g, "")
+		});
+	});
 
-    $("#range-03-text").on('change',function() {
-        var val = Math.round($("#range-03-text").val());
-        $("#range-03-text").val(val);
-        $("#range-03").data("ionRangeSlider").update({
-            from: val
-        });
+	$("#range-03-text").on('change', function () {
+		var val = Math.round($("#range-03-text").val());
+		$("#range-03-text").val(val);
+		$("#range-03").data("ionRangeSlider").update({
+			from: val
+		});
 	});
 
 	calculate()
 });
 
-$(window).resize(function() {
-    if ($(window).width() < 621) {
+$(window).resize(function () {
+	if ($(window).width() < 621) {
 		$('.requirements_btn--link_1').addClass('requirements_btn--link_1--mob');
 		$('.requirements_btn--link_2').addClass('requirements_btn--link_2--mob');
-    } else {
+	} else {
 		$('.requirements_btn--link_1').removeClass('requirements_btn--link_1--mob');
 		$('.requirements_btn--link_2').removeClass('requirements_btn--link_2--mob');
 	}
@@ -96,11 +83,11 @@ $(window).resize(function() {
 }).resize();
 
 function calculate() {
-	var monthly = Math.pow(1.13, 1/12) - 1;
+	var monthly = Math.pow(1.13, 1 / 12) - 1;
 	var s = Math.round((monthly * Math.pow(1 + monthly, range_3_val)) / (Math.pow(1 + monthly, range_3_val) - 1) * range_2_val).toLocaleString()
 	var b = $("#range-02").data("ionRangeSlider").result.from_pretty,
 		m = $("#range-03").data("ionRangeSlider").result.from
-	
+
 	$("#range-02-text").val(b);
 	$("#range-03-text").val(m);
 	$('#range-calc').text(s)
@@ -109,3 +96,24 @@ function calculate() {
 	$("#calc-month").text(m)
 	$('#calc-result').text(s)
 }
+
+
+//  Ajax Contact Form
+
+$(function () {
+	$('#send-1, #send-2, #send-3, #send-4, #send-5, #send-6, #send-7').on('submit', function () {
+		var formID = $(this).attr('id');
+		var formNm = $('#' + formID);
+		$.ajax({
+			type: 'post',
+			url: 'mail/contact.php',
+			data: $(this).serialize(),
+			success: function () {
+				swal("Спасибо за ваш интерес, мы с вами свяжемся в ближайшее время!");
+				$(".modal").modal('hide');
+			}
+		});
+		return false;
+	});
+});
+
---
+++ mail/contact.php
@@ -0,0 +1,44 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = "";
+
+    if (!empty($_POST['formData'])) {$message .= "" . $_POST['formData'] . "\n\n";}
+    if (!empty($_POST['name'])) {$message .= "ФИО: " . $_POST['name'] . "\n\n";}
+    if (!empty($_POST['phone'])) {$message .= "Контактный телефон: " . $_POST['phone'] . "\n\n";}
+    if (!empty($_POST['email'])) {$message .= "E-mail: " . $_POST['email'] . "\n\n";}
+
+    switch ($_POST['select']) {
+        case 1:
+            $message .= "Залог: " . 'Квартира' . "\n\n";
+            break;
+        case 2:
+            $message .= "Залог: " . 'Апартаменты' . "\n\n";
+            break;
+        case 3:
+            $message .= "Залог: " . 'Дом' . "\n\n";
+            break;
+        case 4:
+            $message .= "Залог: " . 'Офис' . "\n\n";
+            break;
+        case 5:
+            $message .= "Залог: " . 'Торговая площадь' . "\n\n";
+            break;
+    }
+
+    if (!empty($_POST['inn'])) {$message .= "ИНН: " . $_POST['inn'] . "\n\n";}
+    if (!empty($_POST['address'])) {$message .= "Адрес: " . $_POST['address'] . "\n\n";}
+    if (!empty($_POST['square'])) {$message .= "Площадь: " . $_POST['square'] . "\n\n";}
+    if (!empty($_POST['cost'])) {$message .= "Рыночная стоимость: " . $_POST['cost'] . "\n\n";}
+
+
+    $to = "business@rusnarbank.ru, smmagic.online@gmail.com";
+    $subject = 'Форма Заявки';
+    $headers = 'From: ' $_POST['email'] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    $send = mail($to, $subject, $message, $headers);
+
+} else {
+	echo 'OK';
+}
+?>
